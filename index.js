395d927055d04701358650d1f31744f8d014dca6
Jeremy Kithome <jerekith@gmail.com>
2020-08-17 20:48:36 +0300

    feat: update typeDefs

--- index.js
+++ index.js
@@ -2,11 +2,55 @@ const { PrismaClient } = require('@prisma/client');
 const { GraphQLServer } = require('graphql-yoga')
 const { makeExecutableSchema } = require('@graphql-tools/schema');
 
+
 const typeDefs = `
-  type Query {
-    hello(name: String): String!
+  type User {
+    id          :ID!
+    name        :String!
+    polls       :[Poll]
   }
-`
+  type Poll {
+    id          :ID!
+    description :String!
+    user        :User!
+    options     :[Option!]
+    votes       :[Vote]
+  }
+  type Option {
+    id          :ID!
+    text        :String!
+    poll        :Poll!
+    votes       :[Vote] 
+  }
+  type Vote {
+    id          :ID!
+    user        :User!
+    poll        :Poll!
+    option      :Option!
+  }
+  type Query {
+    users: [User]
+    polls: [Poll]
+    votes: [Vote]
+    user(id: ID!): User
+    poll(id: ID!): Poll
+  }
+  type Mutation {
+    createUser(
+      name: String!
+    ): User
+    createPoll(
+      description: String!
+      id: ID!
+      options: [String!]
+    ): Poll
+    createVote(
+      userID: ID!
+      pollID: ID!
+      optionID: ID!
+    ): Vote
+  }
+`;
 
 const resolvers = {
   Query: {
